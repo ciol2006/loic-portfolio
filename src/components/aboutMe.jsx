90674fef86721da851f585871e19846a7b4c2833
ciol2006 <ciol2006@gmail.com>
2022-02-20 13:01:29 -0800

    updated about me text

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -4,23 +4,32 @@ const AboutMe = () => {
   return (
     <body className="about-me-page">
       <div className="aboutMe-text-container">
-        <p>My Name is Loïc,</p>{" "}
+        <p>My name is Loïc.</p>{" "}
         <p>
-          I'm an entousiatht and happy learner. Coding for me came on my early
-          days, as my older brother went to university for computer science and
-          getting later on a bit more specialized into 3D modeling. I was always
-          trying to look over is sholder to learn a bit more about it myself.
+          I'm an enthusiastic and motivated learner. I became interested in
+          coding and programming at a young age, as my older brother went to
+          university for computer science and later he specialized in 3D
+          modeling. I was always trying to look over his shoulder to learn a bit
+          more about it myself.
         </p>{" "}
         <p>
-          Two years ago I got inspire from a video on Youtube about seft taught
-          dev that explain there journey. From then on I knew that I was going
-          to be a dev one day.
+          Two years ago I was inspired by a Youtube video about a self-taught
+          dev who explained their professional and personal journey. From that
+          moment I knew that I would be a dev one day.
         </p>{" "}
         <p>
-          I spend a couple of months working on my skills throuh
-          FreeCodeCamp.com, CodeWithMosh.com, Stack Overflow, Youtube. And I
-          love it!
+          I spent a couple of months working on my skills throuh
+          FreeCodeCamp.com, CodeWithMosh.com, Stack Overflow, Youtube.
         </p>
+        <p>
+          I consistently challenge myself and solve problems on my own to
+          improve my skills, only looking for support if necessary.
+        </p>{" "}
+        <p>
+          I am looking for my first job in a team environment with a mentor who
+          can support me in further professional development.
+        </p>
+        <p>The challenge of coding is for me fun and I love it!</p>
         <p>Technologies learned:</p>
       </div>
 
