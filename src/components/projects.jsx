b849d15cabbdbd61ded94b9aa6c6a7f335af31c6
Loic Mas <ciol2006@gmail.com>
2022-03-28 14:09:57 -0700

    Tic_tac_toe project image added

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -5,22 +5,20 @@ const Projects = () => {
     <body className="project-container">
       <div className="frontPage-container">
         <h1>Projects</h1>
-        <div className="projects_grid">
+        <ul className="projects_grid">
           <li>
             <a
               href="https://github.com/ciol2006/Tic-Tac-Toe"
               title="Tic Tac Toe game"
               target="_blank"
             >
-              <i className="Tic_Tac_Toe"></i>
+              <i className="tic_Tac_Toe"></i>
             </a>
           </li>
           <li>
-            <a href="">
-              <h2>Work in progress</h2>
-            </a>
+            <h2>Work in progress, for the second project</h2>
           </li>
-        </div>
+        </ul>
       </div>
     </body>
   );
